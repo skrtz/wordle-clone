4277207b2225215d1e47cbb915f261704899a420
Stephen Kurtz <stephenjkurtz@gmail.com>
2022-02-08 12:13:43 -0500

    import styled-component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 import './App.css';
 
 function App() {
@@ -10,22 +11,52 @@ function App() {
       for(let i = 0; i < 6; i++) {
         newWordGrid.push([])
       }
+
       for(let i = 0; i < 6; i++){
         for(let j = 0; j < 5; j++){
-          newWordGrid[i].push({letter: "", state: "empty"}) //states: "correct", "incorrect", "wrong postition", "empty"
+          newWordGrid[i].push({ letter: "a", state: "empty" }) //states: "correct", "incorrect", "wrong postition", "empty"
         }
       }
+      setWordGrid(newWordGrid);
     }
-    if(wordGrid === 0){
+    
+    // if(wordGrid === 0){
       initializeWordGrid()
-    }
+    // }
   })
 
   return (
-    <div className="App">
-      hello
-    </div>
-  );
+    <Div>
+      {wordGrid.map((row) => (
+        <RowWrapper>
+          {row.map((col) => (
+            <Letter>{col.letter}</Letter>
+          ))}
+        </RowWrapper>
+      ))}
+    </Div>
+
+  )
 }
+
+const Div = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+
+const RowWrapper = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const Letter = styled.div`
+  font-size: 56px;
+  background-color: grey;
+  padding: 8px;
+
+`;
 
 export default App;
